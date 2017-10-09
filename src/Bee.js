afca79e717d98908154580fddcf6c21975da1d88
Hack Reactor Student <student@hackreactor.com>
2017-10-09 12:05:54 -0500

    wrote 'Grub' and 'Bee'

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  // console.log('Grub.prototype', Grub.prototype)
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype)
+
+
+Bee.prototype.constructor = Bee;
